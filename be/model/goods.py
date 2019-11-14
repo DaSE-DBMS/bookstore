c19635899b4be0160e4ad23607bda1a3f7b9e7c1
XuRui <XuRui@DESKTOP-O9T02HG>
2019-11-14 22:06:58 +0800

    update editGoods

--- be/model/goods.py
+++ be/model/goods.py
@@ -3,24 +3,22 @@ from be.model import store
 import jwt
 import time
 
-def jwt_encode(goodsname: str) -> str:
-    encoded = jwt.encode(
-        {"goodsname": goodsname},
-        key=goodsname,
-        algorithm="HS256",
-    )
-    return encoded.decode("utf-8")
-
-
-def jwt_decode(goodsname: str) -> str:
-    decoded = jwt.decode(key=goodsname, algorithms="HS256")
-    return decoded
-
-
 class Goods(tuple.Tuple):
-    goodsname: str
+    username: str
+    goodsid: str
+    goodsName: str
+    goodsPrice: str
+    goodsNumber: str
+
+    def __init__(self, username, goodsId, goodsName, goodsPrice, goodsNumber):
+        self.key = goodsId
+        self.username = username
+        self.goodsName = goodsName
+        self.goodsPrice = goodsPrice
+        self.goodsNumber = goodsNumber
 
 
-    def __init__(self, goodsname):
-        self.key = goodsname
-        self.goodsame = goodsname
+def editGoods(username: str, goodsid: str, goodsName: str, goodsPrice: str, goodsNumber: str) -> bool:
+    g = Goods(username, goodsid, goodsName, goodsPrice, goodsNumber)
+    ok, _ = store.put_row_absent(Goods.__name__, g)
+    return ok
