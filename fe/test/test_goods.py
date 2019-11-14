db1838c4c2c645f2baf88a2752fb3c134c634216
XuRui <XuRui@DESKTOP-O9T02HG>
2019-11-14 22:30:51 +0800

    editGoods

--- fe/test/test_goods.py
+++ fe/test/test_goods.py
@@ -31,3 +31,34 @@
 #     # search wrong merchantname
 #     login_ok, token = m.login(username + "xxx", password, terminal)
 #     assert not login_ok
+import pytest
+import time
+from fe.access import goods
+from fe import conf
+
+
+@pytest.mark.parametrize(
+    "username",
+    [
+        "test_editGoods1_{}".format(time.time()),
+        "test_editGoods2_{}".format(time.time()),
+        "test_editGoods3_{}".format(time.time()),
+    ],
+)
+def test_editGoods(username: str):
+    g = goods.Goods(conf.URL)
+
+    password = "password_" + username
+
+    goodsid = "goodsid_" + username
+
+    goodsName = "goodsname_" + username
+
+    goodsPrice = "goodsPrice_" +username
+
+    goodsNumber = "goodsNumber_" + username
+
+    assert g.editGoods(username, goodsid, goodsName, goodsPrice, goodsNumber)
+
+    assert not a.register(username, goodsid + "_x", goodsName + "_x", goodsPrice + "_x", goodsNumber + "_x")
+
