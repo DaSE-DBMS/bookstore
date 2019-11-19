959950892de0a89f5d3083ce1292a43d4d21f299
ybbh <ybbh@users.noreply.github.com>
2019-11-19 22:35:06 +0800

    logging

--- be/model/store.py
+++ be/model/store.py
@@ -1,19 +1,12 @@
-import os
-from typing import Dict
 import sqlite3 as sqlite
+import logging
 
 
 class Store:
-    tables: Dict[str, table.Table]
     database: str
 
     def __init__(self):
         self.database = "be.db"
-        self.tables = dict()
-        self.create("User")
-        self.create("Order")
-        self.create("Goods")
-        self.create("Carts")
         self.init_tables()
 
     def init_tables(self):
@@ -42,14 +35,8 @@ class Store:
             )
             conn.commit()
         except sqlite.Error as e:
-            print(e)
+            logging.error(e)
             conn.rollback()
-
-    def create(self, table_name: str):
-        self.tables[table_name] = table.Table(table_name)
-
-    def get_table(self, table_name: str) -> table.Table:
-        return self.tables.get(table_name)
 
     def get_db_conn(self) -> sqlite.Connection:
         return sqlite.connect(self.database)
@@ -60,19 +47,3 @@ database_instance = Store()
 
 def get_db_conn():
     return database_instance.get_db_conn()
-
-
-def get_row(table_name: str, key: str) -> tuple.Tuple:
-    return database_instance.get_table(table_name).get(key)
-
-
-def del_row(table_name: str, key: str) -> bool:
-    return database_instance.get_table(table_name).remove(key)
-
-
-def put_row(table_name, row: tuple.Tuple):
-    database_instance.get_table(table_name).put(row)
-
-
-def put_row_absent(table_name, row: tuple.Tuple) -> (bool, tuple.Tuple):
-    return database_instance.get_table(table_name).put_absent(row)
