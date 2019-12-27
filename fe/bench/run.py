d8939c666287d7ed9b3de3caf492551c15e4f497
J0kerMh <jokermh@qq.com>
2019-12-27 13:01:35 +0800

    ss 定义重复,方法重复

--- fe/bench/run.py
+++ fe/bench/run.py
@@ -5,8 +5,6 @@ from fe.bench.session import Session
 def run_bench():
     wl = Workload()
     wl.gen_database()
-    ss = Session(wl)
-    ss.gen_procedure()
 
     sessions = []
     for i in range(0, wl.session):
